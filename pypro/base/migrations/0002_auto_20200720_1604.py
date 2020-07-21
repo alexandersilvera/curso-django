3c8ca82a5d9f180a9f5e9a3dd4f8e5fe04b9204f
alexander <alexandersivera@hotmail.com>
2020-07-21 17:59:52 -0300

    Realizando arreglos con flake8 en proyecto

--- pypro/base/migrations/0002_auto_20200720_1604.py
+++ pypro/base/migrations/0002_auto_20200720_1604.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('auth', '0011_update_proxy_permissions'),
         ('base', '0001_initial'),
@@ -14,6 +13,12 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='user',
             name='groups',
-            field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups'),
+            field=models.ManyToManyField(blank=True,
+                                         help_text='The groups this user belongs to. A user will get all '
+                                         'permissions granted to each of their groups.',
+                                         related_name='user_set',
+                                         related_query_name='user',
+                                         to='auth.Group',
+                                         verbose_name='groups'),
         ),
     ]
--- pypro/modulos/tests/test_aula_detalle.py
+++ pypro/modulos/tests/test_aula_detalle.py
@@ -21,8 +21,10 @@ def resp(client, aula):
     resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
     return resp
 
-def test_titulo(resp, aula:Aula):
+
+def test_titulo(resp, aula: Aula):
     assert_contains(resp, aula.titulo)
 
-def test_vimeo(resp, aula:Aula):
-    assert_contains(resp, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
+
+def test_vimeo(resp, aula: Aula):
+    assert_contains(resp, f'src="https://player.vimeo.com/video/{aula.vimeo_id}"')
