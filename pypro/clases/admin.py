239c18d3605b3f0d0f3fd02512e1f30e8ec975e9
alexander <alexandersivera@hotmail.com>
2020-07-26 23:16:14 -0300

    Ajustes finales de problema en aplicación Clases

--- pypro/clases/admin.py
+++ pypro/clases/admin.py
@@ -1,21 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-<<<<<<< HEAD
-# Register your models here.
-from pypro.clases.models import Clase
 
-=======
 from pypro.clases.models import Clase
 
 
->>>>>>> 37f9113f5b4019ee76cba69763aab8d0c83c302b
 @admin.register(Clase)
 class ClaseAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'slug', 'inicio', 'fin')
     prepopulated_fields = {'slug': ('nombre',)}
-<<<<<<< HEAD
     ordering = ('-inicio',)
-=======
-    ordering = ('-inicio',)
->>>>>>> 37f9113f5b4019ee76cba69763aab8d0c83c302b
+
