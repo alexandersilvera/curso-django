93e7cd01a9beb225ca80aa29ca8dfae6b054dcc4
alexander <alexandersivera@hotmail.com>
2020-09-04 20:47:21 -0300

    Arreglos en heroku para configurar archivos estaticos

--- pypro/settings.py
+++ pypro/settings.py
@@ -182,11 +182,12 @@ if AWS_ACCESS_KEY_ID:
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
 
+    COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
     COLLECTFAST_ENABLED = True
 
     AWS_DEFAULT_ACL = 'public-read'
     AWS_DEFAULT_ACL = 'private'
-    COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strateg'
+
 
     # Static Assets
     # ------------------------------------------------------------------------------
